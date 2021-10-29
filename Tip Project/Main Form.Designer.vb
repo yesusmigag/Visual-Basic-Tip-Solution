6c98eae0b972bef296522dc13c3990dba4df1cb8
yesusmigag <yesusmigag@gmail.com>
2021-10-29 15:28:43 -0400

    Add project files.

---
+++ Tip Project/Main Form.Designer.vb
@@ -0,0 +1,175 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtBill = New System.Windows.Forms.TextBox()
+        Me.txtPercentage = New System.Windows.Forms.TextBox()
+        Me.lblTip = New System.Windows.Forms.Label()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Tip_Project.My.Resources.Resources.Waiter
+        Me.PictureBox1.Location = New System.Drawing.Point(24, 19)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(77, 88)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(36, 118)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(54, 36)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "&Bill:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(117, 118)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(84, 36)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "&Tip %:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(173, 118)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(56, 36)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Tip:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(115, 53)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(251, 51)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Tip Calculator"
+        '
+        'txtBill
+        '
+        Me.txtBill.Location = New System.Drawing.Point(39, 139)
+        Me.txtBill.Name = "txtBill"
+        Me.txtBill.Size = New System.Drawing.Size(75, 42)
+        Me.txtBill.TabIndex = 1
+        '
+        'txtPercentage
+        '
+        Me.txtPercentage.Location = New System.Drawing.Point(120, 139)
+        Me.txtPercentage.Name = "txtPercentage"
+        Me.txtPercentage.Size = New System.Drawing.Size(42, 42)
+        Me.txtPercentage.TabIndex = 3
+        '
+        'lblTip
+        '
+        Me.lblTip.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTip.Location = New System.Drawing.Point(176, 138)
+        Me.lblTip.Name = "lblTip"
+        Me.lblTip.Size = New System.Drawing.Size(75, 25)
+        Me.lblTip.TabIndex = 8
+        Me.lblTip.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(40, 197)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(99, 31)
+        Me.btnCalc.TabIndex = 4
+        Me.btnCalc.Text = "&Calculate tip"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(147, 197)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(99, 31)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.Crimson
+        Me.Label5.Location = New System.Drawing.Point(107, 19)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(343, 36)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Jesus M Alvarado Gonzalez"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(14.0!, 36.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(539, 438)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.lblTip)
+        Me.Controls.Add(Me.txtPercentage)
+        Me.Controls.Add(Me.txtBill)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Restaurant Tip1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtBill As TextBox
+    Friend WithEvents txtPercentage As TextBox
+    Friend WithEvents lblTip As Label
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label5 As Label
+End Class
---
+++ Tip Project/Main Form.vb
@@ -0,0 +1,71 @@
+﻿' Name:         Tip Project
+' Purpose:      Calculate and display a server's tip.
+' Programmer:   Jesus M Alvarado Gonzalez 05/27/2021
+
+
+Public Class frmMain
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        ' Calculates and displays a server's tip.
+        ' Declare Decimal variables.
+
+        Dim decBill As Decimal
+        Dim decPercentage As Decimal
+        Dim decTip As Decimal
+
+        ' Convert txtBill.Text and txtPercentage.Text to numbers.
+
+        Decimal.TryParse(txtBill.Text, decBill)
+        Decimal.TryParse(txtPercentage.Text, decPercentage)
+
+        ' Calculate the tip.
+
+        decTip = decBill * (decPercentage / 100)
+
+        ' Display the tip with a dollar sign and two decimal places.
+
+        lblTip.Text = decTip.ToString("C2")
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ''Close aplication
+        Me.Close()
+
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub txtBill_TextChanged(sender As Object, e As EventArgs) Handles txtBill.TextChanged
+        ''Clear the  bill box
+        lblTip.Text = String.Empty
+
+    End Sub
+
+    Private Sub txtPercentage_TextChanged(sender As Object, e As EventArgs) Handles txtPercentage.TextChanged
+        ''Clear the  Tip box
+        lblTip.Text = String.Empty
+
+    End Sub
+
+    Private Sub txtBill_Enter(sender As Object, e As EventArgs) Handles txtBill.Enter
+
+        txtBill.SelectAll()
+
+    End Sub
+
+    Private Sub txtPercentage_Enter(sender As Object, e As EventArgs) Handles txtPercentage.Enter
+
+        txtPercentage.SelectAll()
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+End Class
